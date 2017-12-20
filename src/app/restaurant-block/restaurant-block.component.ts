426bda68e130c65510183b5adaa600d44849fb37
alisa1234 <alisshapovalova@gmail.com>
2017-12-20 12:49:51 +0200

    last version

--- src/app/restaurant-block/restaurant-block.component.ts
+++ src/app/restaurant-block/restaurant-block.component.ts
@@ -3,7 +3,8 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-restaurant-block',
   templateUrl: './restaurant-block.component.html',
-  styleUrls: ['./restaurant-block.component.scss']
+  styleUrls: ['./restaurant-block.component.scss'],
+  host:{'class':'root'}
 })
 export class RestaurantBlockComponent implements OnInit {
 
